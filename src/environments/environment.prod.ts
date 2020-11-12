55900e978c1857b88057eea06d1c957125935e39
Alejo <alecjoc85@gmail.com>
2020-11-12 20:43:38 -0300

    endpoint security

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endPointSignUp: "http://private-8e8921-woloxfrontendinverview.apiary-mock.com/signup",
-  endPointTechnologyList: "http://private-8e8921-woloxfrontendinverview.apiary-mock.com/techs"
+  endPointSignUp: "https://private-8e8921-woloxfrontendinverview.apiary-mock.com/signup",
+  endPointTechnologyList: "https://private-8e8921-woloxfrontendinverview.apiary-mock.com/techs"
 };
